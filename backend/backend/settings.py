f781d34ad2263f587352b744635fd01a8badf354
Ashutosh pattnaik <ashubtech554@gmail.com>
2026-08-10 13:52:03 +0530

    Prepare Django for production deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ibbn^+6f0-s8!zv*asvc9c65cb92xl*$%v!3jhom!hk#6rpsl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
